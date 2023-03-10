d5674f1b6bbe70ef1a1054308c7f5e4203f862cf
ivan-stankevich <stankevichivann@yandex.by>
2023-03-10 16:12:44 +0100

    feat: numpy finish part 2

--- NumPy_2.py
+++ NumPy_2.py
@@ -18,3 +18,22 @@ print(arr)
 slice_of_array[:] = 99 # при изменении меняются и элементы массива
 print(slice_of_array)
 print(arr)
+arr_copy = arr.copy() # копия
+
+arr_2d = np.array([[5,10,15], [20,25,30], [35,40,45]])
+print(arr_2d)
+print(arr_2d.shape)
+print(arr_2d[0]) # первая строка
+print(arr_2d[2]) # последняя строка
+print(arr_2d[1][1])
+print(arr_2d[1,1])
+print(arr_2d[0,2])
+print(arr_2d[:2])
+print(arr_2d[:2, 1:])
+arr = np.arange(1, 11)
+print(arr>4)
+bool_arr = arr > 4
+print(bool_arr)
+print(arr[bool_arr])
+print(arr)
+print(arr[arr > 4])
